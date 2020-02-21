374e8f3fd8dda12a18dd5f83a32c75f40975f7ca
wesley.moody <wgm3190@gmail.com>
2020-02-21 13:11:27 -0600

    migration built

--- migrations/20200221120844_projects.js
+++ migrations/20200221120844_projects.js
@@ -2,15 +2,15 @@ exports.up = function(knex) {
   return knex.schema
 
     .createTable("tasks", tbl => {
-      tbl
-        .increments()
-        .notNullable()
-        .unsigned()
-        .references("project_tasks")
-        .inTable("projects");
+      tbl.increments();        
       tbl.string("tasks_desc").notNullable();
       tbl.string("tasks_notes");
       tbl.boolean("task_completed").defaultTo("false");
+      tbl.integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects");
     })
 
     .createTable("resources", tbl => {
@@ -24,16 +24,19 @@ exports.up = function(knex) {
     })
 
     .createTable("project_resources", tbl => {
-      tbl.increments();
-      tbl.primary(["project_id", "resource_id"]);
+      // tbl.increments();
+      tbl.primary(["project_id", "resources_id"]);
+      tbl.integer("resources_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")   
       tbl
         .integer("project_id")
         .unsigned()
         .notNullable()
         .references("id")
-        .inTable("projects")
-        .onDelete("RESTRICT");
-      onUpdate("CASCADE");
+        .inTable("projects")        
     })
 
     .createTable("projects", tbl => {
@@ -42,9 +45,7 @@ exports.up = function(knex) {
       tbl.string("project_desc");
       tbl.string("project_resources").notNullable();
       tbl.string("project_tasks").notNullable();
-      tbl
-        .boolean()
-        .notNullable()
+      tbl.boolean("completed")        
         .defaultTo("false");
     });
 };
